99fe5443fdc5f9b2f870b09a4b4fc19e6a1ccffa
Mattias <mattias.ekdahl00@gmail.com>
2025-06-06 17:57:35 +0200

    Add project files.

---
+++ Presentation/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+app.MapOpenApi();
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
